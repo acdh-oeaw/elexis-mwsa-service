355dd1890f2a12073c3f58cb7a9bd353de798315
Seungbin Yim <liam21c@gmail.com>
2021-05-31 21:09:39 +0200

    init image

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ chmod 700 ~/.ssh
 echo "${K8S_SECRET_GITLAB}" > ~/.ssh/id_rsa
 ls -la ~/.ssh
 chmod 700 ~/.ssh/id_rsa
-echo -e "Host gitlab.com\n
+echo "Host gitlab.com\n
       Hostname altssh.gitlab.com\n
       User git\n
       Port 443\n
