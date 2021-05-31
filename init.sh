082920e4ccc4dc5930e5a9fd3973c3fb22b7b4a7
Seungbin Yim <liam21c@gmail.com>
2021-05-31 18:54:24 +0200

    add init

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# This is a comment!
+apk update
+apk add git
+eval $(ssh-agent -s)
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo "${K8S_SECRET_GITLAB}" > ~/.ssh/id_rsa
+ls -la ~/.ssh
+chmod 700 ~/.ssh/id_rsa
+echo -e "Host gitlab.com\n
+      Hostname altssh.gitlab.com\n
+      User git\n
+      Port 443\n
+      PreferredAuthentications publickey\n
+      IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
+cd /builds/acdh-oeaw/elexis/mwsa-service-generated
+ssh -Tvvv -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no git@gitlab.com
+pip install --upgrade pip setuptools wheel
+touch ~/.gitconfig
+pip install "dvc[gs]"==2.0.18
+cp -R models/* /mwsa/models
