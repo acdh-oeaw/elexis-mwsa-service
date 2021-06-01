3d384fe55beaf1eb8d306f907d84331781f52276
Seungbin Yim <liam21c@gmail.com>
2021-06-02 01:48:06 +0200

    test

--- init.sh
+++ init.sh
@@ -7,10 +7,12 @@ ls -la ~/.ssh
 printenv
 echo "${GITLAB}" > ~/.ssh/id_rsa
 echo "${GOOGLE_APPLICATION_CREDENTIALS}" > ~/.creds/gcp.json
-export GOOGLE_APPLICATION_CREDENTIALS=~/.creds/gcp.json
+export GOOGLE_APPLICATION_CREDENTIALS=/root/.creds/gcp.json
+printenv
 cat ~/.ssh/id_rsa
 cat ~/.creds/gcp.json
 chmod 700 ~/.ssh/id_rsa
+chmod 700 /root/.creds/gcp.json
 echo "Host gitlab.com\n
       Hostname altssh.gitlab.com\n
       User git\n
@@ -18,11 +20,11 @@ echo "Host gitlab.com\n
       PreferredAuthentications publickey\n
       IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
 
-ssh -Tvvv -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no git@gitlab.com
+ssh -T -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no git@gitlab.com
 cd /usr/src/app || exit
 pip install --upgrade pip setuptools wheel
 git clone git@gitlab.com:acdh-oeaw/elexis/mwsa-service-generated.git
-cd mwsa-service-generated
+cd mwsa-service-generated || exit
 dvc pull
 #touch ~/.gitconfig
 #pip install "dvc[gs]"==2.0.18
